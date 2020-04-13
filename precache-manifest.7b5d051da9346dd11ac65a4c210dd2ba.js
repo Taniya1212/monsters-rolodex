499cb0a57bdbaf5b9761e45681c4374b3a91d359
Taniya1212 <taniyachaturvedi08@gmail.com>
2020-04-14 01:20:02 +0530

    Updates

---
+++ precache-manifest.7b5d051da9346dd11ac65a4c210dd2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7770ff59ddb87457729bc7bb1e48aa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "75a582c4a568ed3cb16a",
+    "url": "/monsters-rolodex/static/css/main.4adf55ac.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a582c4a568ed3cb16a",
+    "url": "/monsters-rolodex/static/js/main.43aabd52.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
